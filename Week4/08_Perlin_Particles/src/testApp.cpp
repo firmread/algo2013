49d77c723cdea2415339d930e1f6bf5d60ab0561
Charlie Whitney <charlie.whitney@gmail.com>
2013-09-19 07:59:55 -0400

    Adding CodeBlocks examples

--- Week4/08_Perlin_Particles/src/testApp.cpp
+++ Week4/08_Perlin_Particles/src/testApp.cpp
@@ -3,7 +3,9 @@
 //--------------------------------------------------------------
 void testApp::setup(){
     ofBackground(0);
-    
+    ofSetVerticalSync(true);
+    ofSetFrameRate(60);
+
     // make 50 particles up front!
     for( int i=0; i<50; i++ ){
         addParticle();
@@ -17,7 +19,7 @@ void testApp::update(){
     // Note how we don't put it++ up in the top of the loop.
     for( vector<Particle>::iterator it=pList.begin(); it!=pList.end(); ){
         it->update();
-        
+
         if( it->bIsDead ){
             it = pList.erase(it);   // When we erase one, it returns the next particle automatically.  It's done the "it++" for us!
             ofLog( OF_LOG_NOTICE, "size is " + ofToString(pList.size()) );
@@ -36,7 +38,7 @@ void testApp::draw(){
 
 void testApp::addParticle(){
     Particle p;
-    
+
     // we'll pick a random direction for our particle to move initially
     ofVec2f rVel = ofVec2f( ofRandom(1.0), ofRandom(1.0) ) * 10.0 - 5.0;
     p.setup( rVel );
@@ -65,7 +67,7 @@ void testApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
-    
+
     // add 50 more particles on click!
     for( int i=0; i<50; i++ ){
         addParticle();
@@ -88,6 +90,6 @@ void testApp::gotMessage(ofMessage msg){
 }
 
 //--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
+void testApp::dragEvent(ofDragInfo dragInfo){
 
 }
