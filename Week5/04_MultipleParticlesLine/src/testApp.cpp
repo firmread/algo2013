336437dbc4be82bb3b5c9af01506a159cfcd91af
Charlie Whitney <charlie.whitney@gmail.com>
2013-09-25 14:08:30 -0400

    Added in code blocks flies for week 5

--- Week5/04_MultipleParticlesLine/src/testApp.cpp
+++ Week5/04_MultipleParticlesLine/src/testApp.cpp
@@ -5,7 +5,7 @@ void testApp::setup(){
     ofSetVerticalSync(true);
     ofSetFrameRate(60);
     ofBackground(255);
-    
+
 //    for (int i = 0; i < 1000; i++){
 //        addParticle();
 //    }
@@ -13,13 +13,13 @@ void testApp::setup(){
 
 //--------------------------------------------------------------
 void testApp::update(){
-    
+
     // on every frame
 	// we reset the forces
 	// add in any forces on the particle
 	// perfom damping and
 	// then update
-	
+
 	for (int i=0; i < particleList.size(); i++){
 		particleList[i].resetForces();
 //		particleList[i].addForce( ofVec2f(0,0.04));  // gravity
@@ -30,15 +30,15 @@ void testApp::update(){
 
 //--------------------------------------------------------------
 void testApp::draw(){
-    
+
     ofFill();
     ofSetColor(0);
     for (int i=0; i < particleList.size(); i++){
         particleList[i].draw();
     }
-    
-//    ofNoFill();
-    
+
+    ofNoFill();
+
 	ofBeginShape();
 	for (int i = 0; i < particleList.size(); i++){
 		ofVertex(particleList[i].pos.x, particleList[i].pos.y);
@@ -48,87 +48,68 @@ void testApp::draw(){
 
 void testApp::addParticle() {
     Particle myParticle;
-    
+
     float vx = ofRandom(-4,4);
     float vy = ofRandom(-4,4);
     myParticle.setParams(300,300,vx, vy);
-    
-    // more interesting with diversity :)
-    // uncomment this:
-    //		myParticle.damping = ofRandom(0.01, 0.05);
+
     particleList.push_back(myParticle);
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
-    
+
 }
 
 //--------------------------------------------------------------
 void testApp::keyReleased(int key){
-    
+
 }
 
 //--------------------------------------------------------------
 void testApp::mouseMoved(int x, int y ){
-    
-    
+
+
     ofPoint difference = ofPoint(x,y) - lastMousePos;
-    
+
     Particle myParticle;
-    
+
 //    float vx = ofRandom(-4,4);
 //    float vy = ofRandom(-4,4);
     myParticle.setParams(x, y, difference.x, difference.y);
-    
-    // more interesting with diversity :)
-    // uncomment this:
-    //		myParticle.damping = ofRandom(0.01, 0.05);
+
     particleList.push_back(myParticle);
-    
-    
+
+
     lastMousePos.set(x, y);
 }
 
 //--------------------------------------------------------------
 void testApp::mouseDragged(int x, int y, int button){
-    
+
 }
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
-//    for (int i = 0; i < particleList.size(); i++){
-//        //		float vx = ofRandom(-4,4);
-//        //		float vy = ofRandom(-4,4);
-//        
-//        float circVal = ofRandom(TWO_PI);
-//        
-//        float vx = cos( circVal ) * ofRandom(0,4);
-//        float vy = sin( circVal ) * ofRandom(0,4);
-//        
-//		particleList[i].setParams(mouseX,mouseY,vx, vy);
-//        
-//	}
-    
     particleList.clear();
 }
 
 //--------------------------------------------------------------
 void testApp::mouseReleased(int x, int y, int button){
-    
+
 }
 
 //--------------------------------------------------------------
 void testApp::windowResized(int w, int h){
-    
+
 }
 
 //--------------------------------------------------------------
 void testApp::gotMessage(ofMessage msg){
-    
+
 }
 
 //--------------------------------------------------------------
 void testApp::dragEvent(ofDragInfo dragInfo){
-    
+
 }
