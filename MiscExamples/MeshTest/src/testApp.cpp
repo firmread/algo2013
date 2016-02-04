d6df1a4286ab456ac04827c47683ede4866f41bb
firmread <litchirhythm@gmail.com>
2016-02-03 22:56:14 -0500

    init

--- MiscExamples/MeshTest/src/testApp.cpp
+++ MiscExamples/MeshTest/src/testApp.cpp
@@ -2,66 +2,71 @@
 
 //--------------------------------------------------------------
 void testApp::setup(){
-
-    testImage.loadImage("test.png");
+    sampler.loadImage("richard_feynman.jpg");
     
-    int w = testImage.getWidth();
-    int h = testImage.getHeight();
-    
-    
-    // Typically we think of UVs in terms of 0-1, but of defines them as 0-imageWidth
-        
-    mesh.addVertex( ofVec3f(0,100,0) );     // make a new vertex
-    mesh.addTexCoord( ofVec2f(0,h/2) );       // these are our UV coords
-    
-    mesh.addVertex( ofVec3f(0,0,0) );       // make a new vertex
-    mesh.addTexCoord( ofVec2f(0,0) );       // these are our UV coords
-    
-    mesh.addVertex( ofVec3f(100,0,0) );     // make a new vertex
-    mesh.addTexCoord( ofVec2f(w/2,0) );       // these are our UV coords
-    
-    mesh.addVertex( ofVec3f(100,100,0) );   // make a new vertex
-    mesh.addTexCoord( ofVec2f(w/2,h/2) );       // these are our UV coords
-    
-    mesh.addIndex( 0 );
-    mesh.addIndex( 1 );
-    mesh.addIndex( 2 );
-    
-    // you can also just say mesh.addTringle(0,1,2);
-    // if you look at the definition for the funciton, it's just calling addIndex 3 times
-    
-    mesh.addIndex( 2 );
-    mesh.addIndex( 3 );
-    mesh.addIndex( 0 );
-    
+    bMouseIsDown = false;
     ofBackground(0);
-    ofEnableAlphaBlending();
+}
+
+void testApp::addParticle(float x, float y) {
+    
+    // offset the start position a little bit
+    float xPos = x + ofRandom(10) - 5;
+    float yPos = y + ofRandom(10) - 5;
+    
+    // now offset the initial velocity a good amount too
+    ofVec2f velOffset( ofRandom(-1,1), ofRandom(-1,1) );
+    velOffset *= ofRandom(1,5);
+    ofVec2f newVel = mouseVel * 0.3 + velOffset * 0.3;
+    
+    // spawn a new particle with position and velocity based on the mouse with a bit of randomness sprinkled in
+    particleList.push_back( Particle( ofVec2f( xPos, yPos ), newVel ) );
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
+    
+    if( bMouseIsDown ){
+        for( int i=0; i<25; i++){
+            addParticle( lastMousePos.x, lastMousePos.y );
+        }
+    }
+    
+    repulseParticles();
+    
+    for( vector<Particle>::iterator it=particleList.begin(); it!=particleList.end(); ){
+        if( it->bIsDead ){
+            it = particleList.erase( it );
+        }else{
+            it->update( sampler, lastMousePos );
+            it++;
+        }
+    }
+}
 
+void testApp::repulseParticles() {
+        for( vector<Particle>::iterator p1 = particleList.begin(); p1 != particleList.end(); ++p1 ) {
+            vector<Particle>::iterator p2 = p1;
+            for( ++p2; p2 != particleList.end(); ++p2 ) {
+                ofVec2f dir = p1->pos - p2->pos;
+                
+                float distSqrd = dir.lengthSquared();
+                if( distSqrd > 0.0f ){
+                    dir.normalize();
+                    float F = 1.0f/distSqrd;
+                    p1->acc += dir * ( F / p1->mass ) * 0.2;
+                    p2->acc -= dir * ( F / p2->mass ) * 0.2;
+                }
+            }
+        }
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
     
-    ofSetColor( 255 );
-    
-    ofPushMatrix();
-    
-    ofTranslate( ofGetWindowSize() / 2);
-    ofRotateY( ofGetElapsedTimef() * 50 );
-    ofTranslate( ofVec2f(-50, -50) );
-    
-    // when we 'bind' it jsut means that when we draw a mesh next time, use this image
-    testImage.getTextureReference().bind();
-    mesh.draw();
-    
-    // the unbind is very important!
-    testImage.getTextureReference().unbind();
-    
-    ofPopMatrix();
+    for( vector<Particle>::iterator it=particleList.begin(); it!=particleList.end(); it++ ){
+        it->draw();
+    }
 }
 
 //--------------------------------------------------------------
@@ -76,22 +81,24 @@ void testApp::keyReleased(int key){
 
 //--------------------------------------------------------------
 void testApp::mouseMoved(int x, int y ){
-
+    mouseVel = ofVec2f(x,y) - lastMousePos;
+    
+    lastMousePos.set(x, y);
 }
 
 //--------------------------------------------------------------
 void testApp::mouseDragged(int x, int y, int button){
-
+    mouseMoved(x, y);
 }
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
-
+    bMouseIsDown = true;
 }
 
 //--------------------------------------------------------------
 void testApp::mouseReleased(int x, int y, int button){
-
+    bMouseIsDown = false;
 }
 
 //--------------------------------------------------------------
